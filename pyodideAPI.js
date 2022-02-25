aae8d742b7d1223bea93eb56f2721192f49c3081
mostlyaman <amansharma110603@gmail.com>
2022-02-25 19:59:35 +0530

    deploy

---
+++ pyodideAPI.js
@@ -0,0 +1,47 @@
+//Source: https://pyodide.org/en/stable/usage/webworker.html#the-worker-api
+const baseURL = import.meta.env.BASE_URL
+
+const pyodideWorker = new Worker(`${baseURL}worker.js`);
+
+pyodideWorker.postMessage({name: 'baseURL', data: baseURL})
+
+const callbacks = {};
+
+pyodideWorker.onmessage = (event) => {
+  if(event.data.name === "loading-pyodide"){
+    document.dispatchEvent(new CustomEvent('loading-pyodide'))
+  }else if(event.data.name === "loading-packages"){
+    document.dispatchEvent(new CustomEvent('loading-packages', {detail: {data: event.data.data}}))
+  }else if(event.data.name === "starting-python"){
+    document.dispatchEvent(new CustomEvent('starting-python'))
+  }else if(event.data.name === "done-loading"){
+    document.dispatchEvent(new CustomEvent('done-loading', {detail: {data: event.data.data}}))
+  }else if(event.data.name === "loading-sympy"){
+    document.dispatchEvent(new CustomEvent('loading-sympy'))
+  }else if(event.data.name === "loading-python-failed"){
+    document.dispatchEvent(new CustomEvent('loading-python-failed', {detail: {error: event.data.error}}))
+  }
+  else if(event.data.name === "output"){
+    const { id, ...data } = event.data.data;
+    const onSuccess = callbacks[id];
+    delete callbacks[id];
+    onSuccess(data);
+  }
+};
+
+const asyncRun = (() => {
+  let id = 0; // identify a Promise
+  return (script, context) => {
+    // the id could be generated more carefully
+    id = (id + 1) % Number.MAX_SAFE_INTEGER;
+    return new Promise((onSuccess) => {
+      callbacks[id] = onSuccess;
+      pyodideWorker.postMessage({name: "input", data: {
+        ...context,
+        python: script,
+        id,
+      }});
+    });
+  };
+})();
+export default asyncRun ;
---
+++ worker.js
@@ -0,0 +1,62 @@
+// Source: https://pyodide.org/en/stable/usage/webworker.html
+
+
+
+const pyodideVersion = "0.19.0"
+let baseURL = "/"
+
+
+
+importScripts(`https://cdn.jsdelivr.net/pyodide/v${pyodideVersion}/full/pyodide.js`);
+async function loadPyodideAndPackages() {
+  postMessage({name: 'loading-pyodide'})
+  self.pyodide = await loadPyodide({
+    indexURL: `https://cdn.jsdelivr.net/pyodide/v${pyodideVersion}/full/`,
+  });
+  postMessage({name: 'loading-packages', data: pyodideVersion})
+  await self.pyodide.loadPackage(["numpy", "sympy", "micropip", "docutils", 'six']);
+  postMessage({name: "starting-python"})
+  try{
+
+    await self.pyodide.runPythonAsync(`
+import micropip
+from pyodide import to_js
+await micropip.install('${baseURL}gammaLogic-1.0-py3-none-any.whl')
+print("Installed Packages:\\n", micropip.list())
+from gammaLogic.logic import SymPyGamma
+s = SymPyGamma()
+`)
+  }catch(error){
+    console.error(error.message)
+    postMessage({name: 'loading-python-failed', error: error.message})
+  }
+  postMessage({name: 'loading-sympy'})
+  let sympy_version = await self.pyodide.runPythonAsync(`
+import sympy
+to_js(sympy.__version__)
+`)
+  postMessage({name: 'done-loading', data: sympy_version})
+}
+let pyodideReadyPromise = loadPyodideAndPackages();
+
+self.onmessage = async (event) => {
+  if(event.data.name === "baseURL"){
+    baseURL = event.data.data
+  }else if(event.data.name === "input"){
+    // make sure loading is done
+    await pyodideReadyPromise;
+    // Don't bother yet with this line, suppose our API is built in such a way:
+    const { id, python, ...context } = event.data.data;
+    // The worker copies the context in its own "memory" (an object mapping name to values)
+    for (const key of Object.keys(context)) {
+      self[key] = context[key];
+    }
+    // Now is the easy part, the one that is similar to working in the main thread:
+    try {
+      let results = await self.pyodide.runPythonAsync(`to_js(s.eval("`+python+`"))`);
+      self.postMessage({name: "output", data: { results, id }});
+    } catch (error) {
+      self.postMessage({name: "output", data: { error: error.message, id }});
+    }
+  }
+};
